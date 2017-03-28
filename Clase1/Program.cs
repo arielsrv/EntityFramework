cc5525a1c6feeb5242677878a5748de37eea6541
Ariel Piñeiro <arielsrv@gmail.com>
2017-03-28 19:06:52 -0300

    Agrego solución para Visual Studio.

--- Clase1/Program.cs
+++ Clase1/Program.cs
@@ -2,97 +2,94 @@ using System;
 using System.Collections.Generic;
 using static System.Console;
 
-namespace EF 
+namespace EF
 {
-    
-public static class DoubleExtensions
-{        
-    public static double Sin(this double value)
+    public static class DoubleExtensions
     {
-        return Math.Sin(value);
-    }
-}
-
-public static class StringExtension
-{   
-    public static string Reverse(this string value) 
-    {
-        return value;
-    }
-}
-
-class Clase1
-{
-    static void Main(string[] args)
-    {
-        List<int> values = new List<int> {1, 2, 3, 4, 5};
-
-        foreach (int item in values) 
+        public static double Sin(this double value)
         {
-            Console.WriteLine(item);
-        }
-
-        MyCollection<int> numbers = new MyCollection<int>();
-        numbers.Add(1);
-        //numbers.Add("Hello world!"); // Esta línea no compila porque fue declarada cómo Int.32.
-
-        string x = "cadena 1";
-        string y = "cadena 2";
-
-        // Método genérico (no necesariamente implica una clase genérica)
-        Swap<string>(x, y);
-
-        double result = 123;
-        Console.WriteLine(result.Sin());
-
-        //string reverse = "hello".Reverse();
-        //string reverse = StringExtension.Reverse("hello");
-
-        int i  = 123;
-
-        var a = 123;
-        
-        var cadena = default(string);
-        cadena = string.Empty;
-
-        // cadena = 123; "NO COMPILA"
-
-        List<List<int>> myList1 = new List<List<int>>();
-        var myList2 = new List<List<int>>();
-
-        dynamic value = "Hello world";
-        value.RompeTodo();
-
-        value = 123;
-
-        var customer = new
-        {
-            Id = 123,
-            FirstName = "Jorge",
-            LastName = "Sanchez"          
-        };
-        //Customer customer = new Customer(123, "Jorge Sanchez");
-
-        WriteLine(customer.Id);
-        WriteLine(customer.FirstName);
-        WriteLine(customer.LastName);        
-
-        MyType myType = new MyType();
-        myType.X();
-    }
-
-    static void Swap<T>(T x, T y) 
-    {
-        
-    }
-
-    class MyCollection<T>
-    {
-        public void Add(T value)
-        {
-            //TBD
+            return Math.Sin(value);
         }
     }
-}
 
+    public static class StringExtension
+    {
+        public static string Reverse(this string value)
+        {
+            return value;
+        }
+    }
+
+    internal class Clase1
+    {
+        private static void Main(string[] args)
+        {
+            List<int> values = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (int item in values)
+            {
+                Console.WriteLine(item);
+            }
+
+            MyCollection<int> numbers = new MyCollection<int>();
+            numbers.Add(1);
+            //numbers.Add("Hello world!"); // Esta línea no compila porque fue declarada cómo Int.32.
+
+            string x = "cadena 1";
+            string y = "cadena 2";
+
+            // Método genérico (no necesariamente implica una clase genérica)
+            Swap<string>(x, y);
+
+            double result = 123;
+            Console.WriteLine(result.Sin());
+
+            //string reverse = "hello".Reverse();
+            //string reverse = StringExtension.Reverse("hello");
+
+            int i = 123;
+
+            var a = 123;
+
+            var cadena = default(string);
+            cadena = string.Empty;
+
+            // cadena = 123; "NO COMPILA"
+
+            List<List<int>> myList1 = new List<List<int>>();
+            var myList2 = new List<List<int>>();
+
+            dynamic value = "Hello world";
+            value.RompeTodo();
+
+            value = 123;
+
+            var customer = new
+            {
+                Id = 123,
+                FirstName = "Jorge",
+                LastName = "Sanchez"
+            };
+            //Customer customer = new Customer(123, "Jorge Sanchez");
+
+            WriteLine(customer.Id);
+            WriteLine(customer.FirstName);
+            WriteLine(customer.LastName);
+
+            MyType myType = new MyType();
+            myType.X();
+        }
+
+        private static void Swap<T>(T x, T y)
+        {
+        }
+
+        private class MyCollection<T>
+        {
+            public void Add(T value)
+            {
+                //TBD
+            }
+        }
+    }
 }
