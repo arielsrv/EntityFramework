6f00ce2b40afa7625fbc5046bf4bec15a114880e
Ariel Piñeiro <arielsrv@gmail.com>
2017-03-28 20:22:01 -0300

    Agregamos expresiones lambda y delegados generics (action y func).

---
+++ Clase2/DelegadosGenericos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DelegadosGenericos
+{
+    public class Program
+    {        
+        private static void Main(string[] args)
+        {
+            Func<int, int, int> f = (a, b) => a + b;
+            int result = f(2, 3);
+
+            Action<int> print = (value) => Console.WriteLine(value);
+            print(result);            
+        }        
+    }
+}
---
+++ Clase2/Funciones/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Funciones
+{
+    public class Program
+    {
+        private delegate int MyDelegate(int a, int b);
+
+        private delegate void Print(int value);
+
+        private static void Main(string[] args)
+        {
+            MyDelegate f = (a, b) => a + b;
+            int result = f(2, 3);
+
+            Print print = (value) => Console.WriteLine(value);
+            print(result);
+        }
+    }
+}
