df0459806def73054a4b1edc7bbe3c6fce0c586a
Ariel Piñeiro <arielsrv@gmail.com>
2017-03-28 19:46:08 -0300

    Borro las variables.

--- Clase2/Delegados/Program.cs
+++ Clase2/Delegados/Program.cs
@@ -5,14 +5,11 @@ namespace Delegados
 {
     class Program
     {
-        delegate int MyDelegate (int a, int b);
+        delegate int MyDelegate(int a, int b);
         delegate void Print(int value);
 
         static void Main(string[] args)
         {
-            int a = 2;
-            int b = 3;
-
             MyDelegate f = Sum;
 
             int result = f(2, 3);
@@ -21,7 +18,7 @@ namespace Delegados
 
             print(result);
         }
-        
+
         static int Sum(int a, int b)
         {
             return a + b;
