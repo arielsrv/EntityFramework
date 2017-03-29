cad49cf9398f3c506815b55a28e7d9ea08bcc933
Ariel Piñeiro <arielsrv@gmail.com>
2017-03-28 21:12:05 -0300

    Uso de Enumerator con un While (mientras).

---
+++ Clase2/Colecciones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = new List<int>();
+            values.Add(1);
+            values.Add(4);
+            values.Add(9);
+            values.Add(10);
+            values.Add(3);
+
+            foreach (var item in values)
+            {
+                Console.WriteLine(item);
+            }
+
+            IEnumerator<int> enumerator = values.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                int current = enumerator.Current;
+                Console.WriteLine(current);
+            }
+        }
+    }
+
+
+}
