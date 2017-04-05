21de669bc6666a50ccc26f4fb7501011d7bef248
Ariel Piñeiro <arielsrv@gmail.com>
2017-04-04 21:31:58 -0300

    Ejemplo de Fluent API con LinQ y nuestros metodos de extension.

--- Clase3/ConsoleApp/Program.cs
+++ Clase3/ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Collections;
 using System;
 using static System.Console;
+using System.Linq;
 
 namespace ConsoleApp
 {
@@ -9,14 +10,53 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             MyCollection<int> collection = new MyCollection<int> { 2, 5, 1, 19, 10, 3 };
-
             foreach (var item in collection)
             {
                 WriteLine(item);
             }
 
+            WriteLine("\nEjercicios:\n");
+
             int first = collection.First();
-            WriteLine($"Primer elemento: {first}");
+            WriteLine($"\tPrimer elemento: {first}");
+
+            int last = collection.Last();
+            WriteLine($"\tUltimo elemento: {last}");
+
+            int n = 3;
+            var result1 = collection.From(n);
+            WriteLine($"\tA partir de: {n}");
+            foreach (var item in result1)
+            {
+                WriteLine(item);
+            }
+
+            Func<int, int> f1 = x => x * 2;
+            WriteLine($"\tColección modificada:");
+            var result2 = collection.Map(f1);
+            foreach (var item in result2)
+            {
+                WriteLine(item);
+            }
+
+            Func<int, int, int> f2 = (total, item) => total = total + item; 
+            var result3 = collection.Reduce(f2);
+            WriteLine($"\tReducción de la colección: {result3}");
+
+            Func<int, bool> f3 = x => x > 3;
+            WriteLine($"\tColección filtrada:");
+            var result4 = collection.Find(f3);
+            foreach (var item in result4)
+            {
+                WriteLine(item);
+            }
+
+            var result5 = collection
+                .From(1)
+                .Find(x => x > 2)
+                .Map(x => x * 2)
+                .Reduce((total, item) => total = total + item);
+            WriteLine($"\tBienvenidos al mundo de LinQ: {result5}");           
         }
     }
 }
--- Clase3/Extensions/MyCollectionExtensions.cs
+++ Clase3/Extensions/MyCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Collections;
+using System;
 
 namespace Collections
 {
@@ -12,6 +13,71 @@ namespace Collections
             {
                 result = item;
                 break;
+            }
+
+            return result;
+        }
+
+        public static T Last<T>(this MyCollection<T> collection)
+        {
+            T result = default(T);
+
+            foreach (var item in collection)
+            {
+                result = item;
+            }
+
+            return result;
+        }
+
+        public static MyCollection<T> From<T>(this MyCollection<T> collection, int from)
+        {
+            MyCollection<T> result = new MyCollection<T>();
+
+            int current = 0;
+            foreach (var item in collection)
+            {
+                if (current++ < from)
+                    continue;
+                result.Add(item);
+            }
+
+            return result;
+        }
+
+        public static MyCollection<T> Map<T>(this MyCollection<T> collection, Func<T, T> predicate)
+        {
+            MyCollection<T> result = new MyCollection<T>();
+
+            foreach (var item in collection)
+            {
+                T output = predicate(item);
+                result.Add(output);
+            }
+
+            return result;
+        }
+
+        public static T Reduce<T>(this MyCollection<T> collection, Func<T, T, T> predicate)
+        {
+            T result = default(T);
+
+            foreach (var item in collection)
+            {
+                result = predicate(result, item);
+            }
+
+            return result;
+        }
+
+        public static MyCollection<T> Find<T>(this MyCollection<T> collection, Func<T, bool> predicate)
+        {
+            MyCollection<T> result = new MyCollection<T>();
+
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                    result.Add(item);
             }
 
             return result;
