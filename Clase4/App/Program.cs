cc613a146d79fa655199fc00d7925e4e11ddb636
Ariel Piñeiro <arielsrv@gmail.com>
2017-04-11 21:47:19 -0300

    Ejemplo de EF (base de datos existente y usuarios diseñadores)-

---
+++ Clase4/App/Program.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using System;
+using System.Linq;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NorthwindDb db = new NorthwindDb())
+            {
+                var category = new Category
+                {
+                    CategoryName = "Chinese Food",
+                    Description = "Food from china city",
+                };
+
+                db.Categories.Add(category);
+                db.SaveChanges();                
+            }
+        }
+    }
+}
